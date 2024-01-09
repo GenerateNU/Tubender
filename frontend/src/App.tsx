713ab1fafa4c14b11fe0c22886d5dbdab31ce051
hmcclew <122323895+hmcclew@users.noreply.github.com>
2024-01-09 15:56:59 -0500

    configuring tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import './App.css';
 import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Tubender Bare Bones </h1>
+    <div className="flex justify-center">
+      <h1 className='text-4xl'> Tubender Bare Bones </h1>
     </div>
   );
 }
