39a60ba7ed01677373f7fa7f9c4f358c0fedb6c0
michelleli04 <michelle6465li@gmail.com>
2024-02-01 11:29:02 -0500

    created the tangent_vectors file

---
+++ backend/src/tangent_vectors_geom_bspline.cpp
@@ -0,0 +1,16 @@
+
+#include <Geom_Curve.hxx>
+#include <Geom_BSplineCurve.hxx>
+#include <GeomLProp_SLProps.hxx>
+#include <gp_Pnt.hxx>
+#include <gp_Vec.hxx>
+#include <vector>
+
+// outputs a list of vectors containing the tangents to the points on the curve
+std::vector<gp_Vec> calculate_tangent_vectors(const Handle(Geom_BSplineCurve)& bsplineCurve) {
+
+}
+
+int main(int argc, char* argv[]) {
+    return 1;
+}
