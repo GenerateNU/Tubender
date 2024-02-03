0e9b58f62daf7f86e40d0b9ad0e6fb52b8cbee0a
Siddharth Gaikwad <gaikwadsid@Siddharths-MacBook-Pro.local>
2024-02-03 12:29:46 -0500

    Calculates the tangent vectors in a bsplineCurve?

--- backend/src/tangent_vectors_geom_bspline.cpp
+++ backend/src/tangent_vectors_geom_bspline.cpp
@@ -8,7 +8,18 @@
 
 // outputs a list of vectors containing the tangents to the points on the curve
 std::vector<gp_Vec> calculate_tangent_vectors(const Handle(Geom_BSplineCurve)& bsplineCurve) {
-
+    std::vector<gp_Vec> tangentVectors;
+    std::gp_Vec tangentVector;
+    std::gp_Pnt point;
+    int knots = bsplineCurve->NbKnots();
+    TColStd_Array1OfReal knotsArray(1, knots);
+    bsplineCurve->Knots(knotsArray);
+    for (int i = 0; i < knots; i++) {
+        Standard_Real knot = knotsArray(i);
+        bsplineCurve->D1(knot, point, tangentVector);
+        tangentVectors.push_back(tangentVector);
+    }
+    return tangentVectors;
 }
 
 int main(int argc, char* argv[]) {
