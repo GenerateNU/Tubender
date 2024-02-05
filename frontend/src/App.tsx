e0d3c2707cbc20f194d86921a1b60c15748cb488
nourtawfik04 <tawfik.n@northeastern.edu>
2024-02-05 11:34:19 -0500

    file upload button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import FileUploader from './FileUploader'; 
 
 function App() {
   return (
     <div className="flex justify-center">
-      <h1 className='text-4xl'> Tubender Bare Bones </h1>
+      <h1 className='text-4xl'>Tubender Bare Bones</h1>
+      <FileUploader /> {}
     </div>
   );
 }
