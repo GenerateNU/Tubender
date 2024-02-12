4223ca69a2d68a767d157b0d456dd88cea7e7a59
Siddharth Gaikwad <gaikwadsid@Siddharths-MacBook-Pro.local>
2024-02-12 17:36:03 -0500

    Determing whether file is step or igs file and converting to appropriate shape method complete

--- backend/src/determine_file_type.cpp
+++ backend/src/determine_file_type.cpp
@@ -2,19 +2,52 @@
 #include <iostream>
 #include <string>
 #include <TopoDS_Shape.hxx>
+#include <iostream> 
 #include <filesystem> 
 using namespace std; 
 
 TopoDS_Shape convert_file(const string filepath) { 
-    filesystem::path p(filepath); 
-    string extension = p.extension().string().tolower();
+    std::__fs::filesystem::path p(filepath); 
+    string extension = p.extension().string();
+    for (auto& x : extension) { 
+        x = tolower(x); 
+    } 
+    TopoDS_Shape shape = TopoDS_Shape();
+    cout << "File extension: " << extension << endl;
     if (extension == ".iges" || extension == ".igs") {
+        cout << "Reading IGES file." << endl;
         TopoDS_Shape shape = read_iges(filepath.c_str());
         return shape;
     } else if (extension == ".step" || extension == ".stp") {
+        cout << "Reading Step file." << endl;
         TopoDS_Shape shape = read_step(filepath.c_str());
         return shape;
     } else {
         throw std::runtime_error("Error: File type not supported.");
     }
 }
+
+int main(int argc, char* argv[]) {
+    string step_file = "Full_Frame.STEP";
+    TopoDS_Shape shape = convert_file(step_file);
+
+    return 0; 
+}
+  
+// int main() 
+// { 
+//     // The path to the file 
+//     char* filepath = "C:\\Users\\Drunk\\Desktop\\l.png"; 
+  
+//     // Passing the path as argument to the function 
+//     std::__fs::filesystem::path p(filepath); 
+  
+//     // Displaying filename and extensions separately 
+//     // By accessing the respective constructors 
+//     cout << "filename and extension: " << p.filename() 
+//          << std::endl; 
+//     cout << "filename only: " << p.stem() << std::endl; 
+//     cout << "Extension: " << p.extension(); 
+  
+//     return 0; 
+// }
