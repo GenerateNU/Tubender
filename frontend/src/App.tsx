460b75cd28e95228cefda01912cf505d67095ed7
hmcclew <122323895+hmcclew@users.noreply.github.com>
2024-02-13 19:27:59 -0500

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import FileUploader from './components/FileUploader'; 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import MainPage from './pages/MainPage';
+import PersuasivePage from './pages/PersuasivePage';
+import ContactPage from './pages/ContactPage';
+import ReviewsPage from './pages/ReviewsPage';
 
 function App() {
   return (
-    <div className="flex justify-center">
-      <FileUploader /> {}
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/persuasive" element={<PersuasivePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/reviews" element={<ReviewsPage />} />
+        </Routes>
+    </Router>
+    
   );
 }
 
