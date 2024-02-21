0ad6a94f39f585cd557b55b0ad73512c9573c6b4
hmcclew <122323895+hmcclew@users.noreply.github.com>
2024-02-21 13:16:34 -0500

    updated file uploading component

--- frontend/src/components/FileUploader.tsx
+++ frontend/src/components/FileUploader.tsx
@@ -3,17 +3,12 @@ import React, { ChangeEvent, useState } from 'react';
 const FileUploader: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (files && files[0]) {
       setSelectedFile(files[0]);
       console.log('Selected file:', files[0].name);
-    }
-  };
-
-  const handleFileUpload = () => {
-    if (selectedFile) {
-      alert(`File selected for upload: ${selectedFile.name}`);
+      alert(`File selected for upload: ${files[0].name}`);
     } else {
       alert('No file selected.');
     }
@@ -23,7 +18,7 @@ const FileUploader: React.FC = () => {
     <div>
       <label htmlFor="fileInput" className="w-52 h-12 flex justify-center items-center rounded-xl bg-brand-blue text-brand-white cursor-pointer">
         <h3 className="text-center font-semibold text-sm">Upload Bend Design</h3>
-        <input type="file" id="fileInput" accept=".iges,.step" onChange={handleFileUpload} style={{ display: 'none' }} />
+        <input type="file" id="fileInput" accept=".iges,.step,.igs" onChange={handleFileUpload} style={{ display: 'none' }} />
       </label>
     </div>
   );
