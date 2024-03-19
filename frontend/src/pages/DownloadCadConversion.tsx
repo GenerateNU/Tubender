7577d22baebd5817a20f73ea7f44478b11bfe492
aaronkimbrooktec <218adk@gmail.com>
2024-03-18 23:53:52 -0400

    trigger download with button instead of automatic download

--- frontend/src/pages/DownloadCadConversion.tsx
+++ frontend/src/pages/DownloadCadConversion.tsx
@@ -2,17 +2,34 @@ import React from 'react';
 import Button from '../components/Button';
 import Navbar from '../components/Navbar';
 
+
+const { ipcRenderer } = window.require('electron');
+
 function DownloadCadConversion() {
-  return (
+
+    const handleDownloadClick = () => {
+        ipcRenderer.send('download-file');
+        
+        ipcRenderer.once('download-success', () => {
+          console.log('Download successful');
+        });
+    
+        ipcRenderer.once('download-failure', (event, error) => {
+          console.error('Download failed', error);
+        });
+      };
+
+
+    return (
     <div className="min-h-screen w-full flex flex-col justify-center items-center bg-off-white gap-6">
         <Navbar/>
         <h2 className="font-semibold text-3xl text-brand-blue">Download GCode</h2>
         <div className="w-1/3 flex flex-col justify-center items-center gap-6">
-            <Button label='Download File' handleClick={() => console.log('button')}></Button>
-            <Button label='Upload To Machine' handleClick={() => console.log('button')}></Button>
+            <Button label='Download File' handleClick={handleDownloadClick}></Button>
+            <Button label='Upload To Machine' handleClick={() => console.log('Upload to machine')}></Button>
         </div>
     </div>   
-  );
+    );
 }
 
 export default DownloadCadConversion;
