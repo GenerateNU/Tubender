bee681847763bfb313d0c6f0a2b0288d30cc643f
michelleli04 <michelle6465li@gmail.com>
2024-03-21 18:02:18 -0400

    added wordmark to create design page

--- frontend/src/pages/CreateDesign.tsx
+++ frontend/src/pages/CreateDesign.tsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 import Button from "../components/Button";
 import FileUploader from "../components/FileUploader";
 import StepWrapper from "../components/StepWrapper";
+import Navbar from "../components/Navbar";
 
 function CreateDesign() {
     return (
+        <>
+        <Navbar/>
         <div className='w-full h-screen bg-off-white flex flex-col justify-center items-center gap-4'>
             <StepWrapper title="Welcome">
                 <Link to="/create/custom">
@@ -14,6 +17,7 @@ function CreateDesign() {
                     <FileUploader />
             </StepWrapper>
         </div>
+        </>
     );
 }
 
